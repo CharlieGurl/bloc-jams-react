6454290746c7737e972a20701a43bcd897ba05a9
Charlotte Knepper <charlotteknepper@Charlottes-MacBook-Pro.local>
2018-09-03 13:56:57 -0600

    Checkpoint 8 player bar buttons completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar'; 
 
 
 class Album extends Component {
@@ -47,6 +48,14 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick () {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex -1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   handleMouseEnter(song) {
    this.setState({ isHovered: song }); 
   }
@@ -60,7 +69,7 @@ class Album extends Component {
     if (this.state.isHovered === song && this.state.isPlaying && isSameSong) {
       return <span className="icon ion-md-pause" />;
     } else if (this.state.isHovered === song && !this.state.isPlaying) {
-      return <span className="icon ion-md-play-circle" />;
+      return <span className="icon ion-md-play" />;
     }
 
     return <button className="number">{index + 1} </button>;
@@ -98,6 +107,12 @@ class Album extends Component {
           </tbody>
 
         </table>
+        <PlayerBar
+           isPlaying={this.state.isPlaying}
+           currentSong={this.state.currentSong}
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+         />
       </section>
     );
   }
